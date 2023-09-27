468b2526908777609b30c43c01a167f30be4dd76
callmehawa <thatal.pawan@gmail.com>
2023-09-27 22:59:20 +0100

    added a new code to create a simple dicitionary This dicitionary code can be used by the team to familiarise ourselves with pull the code, amending and commiting it

---
+++ dicitionary.py
@@ -0,0 +1,8 @@
+# Create an empty dictionary
+my_dict = {}
+
+# Create a dictionary with key-value pairs
+dictionary = {"name": "Pawan", "age": 40, "city": "Preston"}
+
+#print dictionary
+print(dictionary)
